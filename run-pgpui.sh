bc36ca70bfa962ffe57bb8c23f23ed81b539de4e
PoBe <you@example.com>
2026-04-26 06:48:51 +0200

    fixes for V1.5

--- run-pgpui.sh
+++ run-pgpui.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java  -cp "lib/*:build/libs/PGP-UI-1.0-SNAPSHOT.jar" com.hoddmimes.gpgui.PGPGUI
+java  -cp ./build/libs/PGP-UI-1.0.jar:lib/bcpg-jdk18on-1.78.1.jar:lib/bcprov-jdk18on-1.78.1.jar com.hoddmimes.gpgui.PGPGUI
